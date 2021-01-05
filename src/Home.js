bcea795d498e6f671ad476e5d4b1c36c60221f6f
spreet2472 <samre2472@gmail.com>
2021-01-05 13:12:57 -0500

    blogSection

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home() {
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    {
+      title: 'Web dev top tips',
+      body: 'lorem ipsum...',
+      author: 'mario',
+      id: 3,
+    },
+  ]);
+
   return (
     <div className='Home'>
-      <h1>Homepage</h1>
+      {blogs.map((blog) => (
+        <div className='blog-preview' key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>written by {blog.author} </p>
+        </div>
+      ))}
     </div>
   );
 }
